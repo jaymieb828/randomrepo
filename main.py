0a3963277493fedf559c00e4f58781488f3532c8
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-05-03 15:21:38 -0400

    added lists to main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-main.py
+#Day Trip Generator
+
+destinations = ["New York", "Orlando",  "Honolulu", "San Diego", "Paris", "Shanghai"]
+
+restaurants = ["McDonald's", "Taco Bell", "KFC", "Albertos Mexican Food", "Pizza hut", "Panda Express"]
+
+transportation = ["Private Jet", "Uber", "Bullet train", "Lambroghini rental car"]  
+
+entertainment = "A Show", "Gator wrestling", "Mountain biking", "Surfing", "Roller Blading", "Sight seeing"
+
