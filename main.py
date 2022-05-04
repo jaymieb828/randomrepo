9d068f55a8265b5c5690b0349d9b394569c2f8a7
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-05-04 14:41:26 -0400

    modified lines 25-30 to become a function MAIN.PY

--- main.py
+++ main.py
@@ -3,9 +3,6 @@
 
 #lists
 
-from random import random
-
-
 # destinations = ["New York", "Orlando",  "Honolulu", "San Diego", "Paris", "Shanghai"]
 
 # restaurants = ["McDonald's", "Taco Bell", "KFC", "Albertos Mexican Food", "Pizza hut", "Panda Express"]
@@ -14,18 +11,20 @@ from random import random
 
 # entertainment = ["A Show", "Gator wrestling", "Mountain biking", "Surfing", "Roller Blading", "Sight seeing"]
 
-
-
 import random
 
 destinations = ["New York", "Orlando",  "Honolulu", "San Diego", "Paris", "Shanghai"]
 
 print ("Welcome to your day trip planner!" + " We have selected " + (random.choice(destinations) + " as your destination. "))
 
-yes_or_no = (input("Are you happy with this choice we made for you?"))
-if "No" or "no":
-    print("I'm sorry to hear that." + " " + "How about" + " " + (random.choice(destinations) + " " + "instead?"))
-elif "Yes" or "yes":
-    print("Thats wonderul, let's move on...")
+# Yes = True
+# yes = True
+# No = False
+# no = False
 
-    
+def yes_or_no  (input("Are you happy with this choice we made for you?")):
+    if yes_or_no is "No" or "no":   
+        print(("I'm sorry to hear that." + " " + "How about" + " " + (random.choice(destinations) + " " + "instead?")))
+
+
+
