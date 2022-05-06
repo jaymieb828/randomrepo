7fa8cbcb7c1d93a4c35a6ea688bf86699b7a8c49
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-05-06 06:27:33 -0400

    Added functions for transportation and activities to MAIN.PY

--- scratchpaper.py
+++ scratchpaper.py
@@ -1,39 +1,52 @@
+
+from gc import collect
 import random
+
 
 destinations = ["New York", "Orlando",  "Honolulu", "San Diego", "Paris", "Shanghai"]
 restaurants = ["McDonald's", "Taco Bell", "KFC", "Albertos Mexican Food", "Pizza hut", "Panda Express"]
 transportation = ["Private Jet", "Uber", "Bullet train", "Lambroghini rental car"]  
 entertainment = ["A Show", "A ballet", "Mountain biking", "Surfing", "Roller Blading", "Sight seeing"]
+confirmation = []
 
-print ("Welcome to your day trip planner!" + " We have selected " + (random.sample(destinations) + " as your destination. "))
+chow = (random.choice(restaurants)) 
+    
+print (chow)
 
-def place(answer): #destination selector
+    
+
+print ("Welcome to your day trip planner!" + " We have selected " + (random.choice(destinations) + " as your destination. "))
+
+def place(answer):
     while True: 
         answer = (input("Are you happy with this destination?"))
         if answer == "yes":
             print("That's a beautiful place to visit!")
+            return confirmation [0]
             break
         elif answer =="no": 
-            (input(("I'm sorry to hear that." + " " + "How about" + " " + (random.sample(destinations)) + " " + "instead?")))
+            (input(("I'm sorry to hear that." + " " + "How about" + " " + (random.choice(destinations)) + " " + "instead?")))
             continue
         else: 
             print("Enter yes or no")
             break   
-   
+
 place("yes" or "no")
 
+
+
 print ("We hope you enjoy your stay.")
-print ("Next we have planned a wonderful meal at" + " " + (random.sample(restaurants) + "."))
+print ("Next we have planned a wonderful meal at" + " " + (random.choice(restaurants) + "."))
 print ("A fine meal to enjoy for sure!")
 
 
-def food(answer): #meal selector
+def food(answer):
     while True: 
         answer = (input("Are you happy with this meal selection?"))
         if answer == "yes":
             break 
-        elif answer == "no": 
-            (input(("I'm sorry to hear that." + " " + "How about" + " " + (random.sample(restaurants)) + " " + "instead?")))
+        elif answer =="no": 
+            (input(("I'm sorry to hear that." + " " + "How about" + " " + (random.choice(restaurants)) + " " + "instead?")))
             continue
         else: 
             print("Enter yes or no")
@@ -42,15 +55,15 @@ def food(answer): #meal selector
 food("yes" or "no")  
 
 print("Excellent choice!")
-print("for your mode of transportation we have chosen" + " " + (random.sample(transportation) + " " + "for you."))
+print("for your mode of transportation we have chosen" + " " + (random.choice(transportation) + " " + "for you."))
         
-def transport(answer): #transportation selector
+def transport(answer):
     while True: 
         answer = (input("Are you happy with this mode of transport?"))
         if answer == "yes":
             break 
         elif answer =="no": 
-            (input(("Agreed, that selection was lame." + " " + "How about" + " " + (random.sample(transportation)) + " " + "instead?")))
+            (input(("Agreed, that selection was lame." + " " + "How about" + " " + (random.choice(transportation)) + " " + "instead?")))
             continue
         else: 
             print("Enter yes or no")
@@ -59,36 +72,18 @@ def transport(answer): #transportation selector
 transport("yes" or "no")
 
 print ("This day trip is looking awesome!")
-print ("Up next, we've selected" + " " + (random.sample(entertainment)) + " " + "for you.")
+print ("Up next, we've selected" + " " + (random.choice(entertainment)) + " " + "for you.")
 
-def fun_stuff(answer): #entertainment selector
+def fun_stuff(answer):
     while True: 
         answer = (input("Are you feeling this choice?"))
         if answer == "yes":
             break 
         elif answer =="no": 
-            (input(("Agreed, that selection was not a good fit." + " " + "How about" + " " + (random.sample(entertainment)) + " " + "instead?")))
+            (input(("Agreed, that selection was not a good fit." + " " + "How about" + " " + (random.choice(transportation)) + " " + "instead?")))
             continue
         else: 
             print("Enter yes or no")
             break   
 
 fun_stuff("yes" or "no")
-
-input("Once you are happy with these selections, please confirm your daytrip as complete by typing 'Complete'")
-if(input == "Complete"):
-    print("We're glad you enjoyed your day trip")
-else:
-    print("Maybe you'll need to hire a better day trip planner.")
-
-
-# #destination function
-
-# # def get_random_destination(rand_des):
-# #     rand_des = random.randrange(len(destinations))
-# #     rand_loc = destinations[rand_des]
-# #     print (rand_loc)
-# #     return
-
-
-
